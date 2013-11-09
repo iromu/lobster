88cbb021ea3f69297bfc3d102c0c36f923706ee3
Carnil4 <carnil@gmail.com>
2013-11-09 06:57:19 +0100

    Added text with the name of the status attribute being displayed

--- rest/src/main/webapp/client/js/displayState.js
+++ rest/src/main/webapp/client/js/displayState.js
@@ -12,16 +12,22 @@ var FRAME_BORDER = 5;
 function createStatusLevelBar(basex, basey, attributeName,value)
 {
     // Draw the name of the attribute
+    var text = new createjs.Text(attributeName, "20px Arial", "#ffbb55");
+    text.x = basex;
+    text.y = basey;
+    text.textBaseline = "alphabetic";
+    stage.addChild(text);
 
     // Draw the bar
     frame = new createjs.Shape();
     // Draw the external frame
-    frame.graphics.beginFill("yellow").drawRect(basex,basey,FRAME_WIDTH, FRAME_HEIGHT);
+    var framey = basey + 15;//text.getMeasuredHeight();
+    frame.graphics.beginFill("yellow").drawRect(basex,framey,FRAME_WIDTH, FRAME_HEIGHT);
     // Draw the internal empty space
-    frame.graphics.beginFill("brown").drawRect(basex+FRAME_BORDER, basey+FRAME_BORDER,
+    frame.graphics.beginFill("brown").drawRect(basex+FRAME_BORDER, framey+FRAME_BORDER,
         FRAME_WIDTH - 2*FRAME_BORDER, FRAME_HEIGHT-2*FRAME_BORDER);
     // Draw the filled bar
-    frame.graphics.beginFill("green").drawRect(basex+FRAME_BORDER, basey+FRAME_BORDER,
+    frame.graphics.beginFill("green").drawRect(basex+FRAME_BORDER, framey+FRAME_BORDER,
         (FRAME_WIDTH - 2*FRAME_BORDER)*value/100, FRAME_HEIGHT-2*FRAME_BORDER);
 
     stage.addChild(frame);
@@ -30,12 +36,14 @@ function createStatusLevelBar(basex, basey, attributeName,value)
 
 function updateState(data, textStatus, jqXHR)
 {
-    alert("updateState");
+    //alert("updateState");
 
     var state = data;
 
     createStatusLevelBar(30, 400, "Total Calories", state.totalCalories);
-    createStatusLevelBar(30, 450, "Ideal Calories", state.idealCalories);
+    createStatusLevelBar(30, 480, "Ideal Calories", state.idealCalories);
+    createStatusLevelBar(30, 560, "Happiness", state.happiness);
+    //createStatusLevelBar(30, 550, "Vitamin A", state.statusVitamineList[0].status)
 
 }
 
@@ -46,7 +54,7 @@ function handleError(  jqXHR,  textStatus,  errorThrown)
 
 function queryState( )
 {
-    alert("queryState");
+    //alert("queryState");
 
     // TMP: For now we will always query the 1st element
     var id = 1;
