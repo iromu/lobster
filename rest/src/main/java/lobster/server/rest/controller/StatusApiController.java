a535b74108246e87d5142d6b126745890c7f1470
FerniMon <hackforedu2013>
2013-11-09 03:56:26 +0100

    Status controller wrong

---
+++ rest/src/main/java/lobster/server/rest/controller/StatusApiController.java
@@ -0,0 +1,35 @@
+package lobster.server.rest.controller;
+
+import lobster.server.rest.model.Activity;
+import lobster.server.rest.persistence.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ferni
+ * Date: 9/11/13
+ * Time: 3:50
+ * To change this template use File | Settings | File Templates.
+ */
+@RequestMapping("/status/")
+@Controller
+public class StatusApiController {
+    @Autowired
+    private ActivityService activityService;
+
+
+
+    @ResponseBody
+    @RequestMapping(value = "getStatus", method = RequestMethod.GET)
+    public List<Activity> getActivities(Integer lobsterID) {
+        return activityService.getAll();
+    }
+
+
+}
