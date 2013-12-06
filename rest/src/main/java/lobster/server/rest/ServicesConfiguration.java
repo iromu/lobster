3ea3f4cdbdffe23eb070ef411697afaaeb58a6fc
iromu <wantez@gmail.com>
2013-12-06 20:20:16 +0100

    JavaConfig

--- rest/src/main/java/lobster/server/rest/ServicesConfiguration.java
+++ rest/src/main/java/lobster/server/rest/ServicesConfiguration.java
@@ -7,10 +7,26 @@ import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaDialect;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 /**
@@ -27,13 +43,79 @@ import java.util.Arrays;
 @EnableTransactionManagement(proxyTargetClass = true)
 @EnableJpaRepositories("lobster.persistence.jpa.repository")
 @ImportResource({"classpath:spring-context.xml"})
+@PropertySource("classpath:jdbc.properties")
 public class ServicesConfiguration {
+
+    @Inject
+    private org.springframework.core.env.Environment environment;
 
     @Bean
     public CacheManager cacheManager() throws Exception {
         SimpleCacheManager scm = new SimpleCacheManager();
         scm.setCaches(Arrays.asList(new ConcurrentMapCache("lobsters"), new ConcurrentMapCache("food")));
         return scm;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("test-db")
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+        lcemfb.setDataSource(dataSource());
+        lcemfb.setJpaDialect(eclipseLinkJpaDialect());
+        lcemfb.setJpaVendorAdapter(jpaVendorAdapter());
+        lcemfb.setPersistenceUnitName("persistenceUnit");
+        lcemfb.getJpaPropertyMap().put("eclipselink.weaving", "false");
+        lcemfb.afterPropertiesSet();
+        return lcemfb.getObject();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        EclipseLinkJpaVendorAdapter jpaVendorAdapter = eclipseLinkJpaVendorAdapter();
+        jpaVendorAdapter.setShowSql(true);
+        jpaVendorAdapter.setDatabase(Database.H2);
+        jpaVendorAdapter.setGenerateDdl(true);
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public EclipseLinkJpaVendorAdapter eclipseLinkJpaVendorAdapter() {
+        return new EclipseLinkJpaVendorAdapter();
+    }
+
+    @Bean
+    public EclipseLinkJpaDialect eclipseLinkJpaDialect() {
+        return new EclipseLinkJpaDialect();
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer() {
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(dataSource());
+        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator());
+        return dataSourceInitializer;
+    }
+
+    @Bean
+    public ResourceDatabasePopulator resourceDatabasePopulator() {
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.setSqlScriptEncoding("UTF-8");
+        resourceDatabasePopulator.addScript(new ClassPathResource(environment.getProperty("jdbc.initLocation")));
+        return resourceDatabasePopulator;
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory());
+        return jpaTransactionManager;
     }
 
 }
