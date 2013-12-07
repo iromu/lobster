4ebfaa9ba0ae94835a71418c40ca681437e99cd2
iromu <wantez@gmail.com>
2013-12-08 00:08:18 +0100

    Test update

--- rest/src/test/java/lobster/server/rest/controller/LobsterApiControllerIT.java
+++ rest/src/test/java/lobster/server/rest/controller/LobsterApiControllerIT.java
@@ -1,41 +1,27 @@
 package lobster.server.rest.controller;
 
-import lobster.persistence.jpa.repository.StatusRepository;
+import lobster.persistence.model.Food;
 import lobster.persistence.model.Lobster;
 import lobster.persistence.model.Status;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created with IntelliJ IDEA.
  * User: wantez
  * Date: 08/11/13
  * Time: 22:30
- *
  */
-public class LobsterApiControllerIT {
+public class LobsterApiControllerIT extends AbstractControllerTest {
 
-    @Autowired
-    private StatusRepository statusRepository;
-
-    private RestTemplate restTemplate;
-
-    @Before
-    public void restTemplate() {
-        this.restTemplate = new RestTemplate();
-        restTemplate.setMessageConverters(Collections.<HttpMessageConverter<?>>singletonList(new MappingJacksonHttpMessageConverter()));
-    }
+    protected static int TOTAL = 6;
 
     @Test
     public void add() {
@@ -43,37 +29,33 @@ public class LobsterApiControllerIT {
         lobster.setName("test");
         lobster.setEmail("email");
 
-        Long id = restTemplate.postForObject("http://localhost:8080/api/lobsters", lobster, Long.class);
+        Long id = restTemplate.postForObject(HTTP_API + "lobsters", lobster, Long.class);
         assertNotNull(id);
+        TOTAL++;
 
-        lobster = restTemplate.getForObject("http://localhost:8080/api/lobsters/{lobsterId}", Lobster.class, id);
-        assertNotNull(lobster);
-        assertNull(lobster.getStatus());
+        lobster = restTemplate.getForObject(HTTP_API + "lobsters/{lobsterId}", Lobster.class, id);
+        assertThat(lobster, notNullValue());
+        assertThat(lobster.getStatus(), notNullValue());
     }
 
     @Test
     public void getAll() {
-
-        ResponseEntity<List> l;
-        l = restTemplate.getForEntity("http://localhost:8080/api/lobsters", List.class);
-
-        System.out.println(l.toString());
+        List<Food> list = restTemplate.getForObject(HTTP_API + "lobsters", List.class);
+        assertThat(list.size(), is(TOTAL));
     }
 
 
     @Test
     public void giveFood() {
-        Status status = restTemplate.postForObject("http://localhost:8080/api/lobster/1/givefood/1", null, Status.class);
-        status = restTemplate.postForObject("http://localhost:8080/api/lobsters/1/givefood/2", null, Status.class);
-
-        assertNotNull(status);
-        assertThat(status.getVitamineAmountList().size(), is(4));
+        Status status = restTemplate.postForObject(HTTP_API + "lobsters/1/givefood/2", null, Status.class);
+        assertThat(status, notNullValue());
+        assertThat(status.getVitamineAmountList().size(), is(3));
 
     }
 
     @Test
     public void doActivity() {
-        restTemplate.postForLocation("http://localhost:8080/api/lobsters/1/doActivity/1", null);
+        restTemplate.postForLocation(HTTP_API + "lobsters/1/doActivity/1", null);
     }
 
 }
--- rest/src/test/java/lobster/server/rest/controller/StatusApiControllerIT.java
+++ rest/src/test/java/lobster/server/rest/controller/StatusApiControllerIT.java
@@ -1,16 +1,13 @@
 package lobster.server.rest.controller;
 
-import lobster.persistence.model.Lobster;
 import lobster.persistence.model.Status;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
-import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -20,24 +17,12 @@ import static org.junit.Assert.assertThat;
  * User: Ferni
  * Date: 9/11/13
  * Time: 4:13
- *
  */
-public class StatusApiControllerIT {
-
-    private RestTemplate restTemplate;
-
-    @Before
-    public void restTemplate() {
-        this.restTemplate = new RestTemplate();
-        restTemplate.setMessageConverters(Collections.<HttpMessageConverter<?>>singletonList(new MappingJacksonHttpMessageConverter()));
-    }
+public class StatusApiControllerIT extends AbstractControllerTest {
 
     @Test
     public void get() {
-        Integer lobsterId = 1;
-       // ResponseEntity<Status> status;
-        Status status = restTemplate.getForObject("http://localhost:8080/api/status/{lobsterId}", Status.class, lobsterId);
-
+        Status status = restTemplate.getForObject(HTTP_API + "status/{lobsterId}", Status.class, 1L);
         assertThat(status.getId(), is(1L));
     }
 }
